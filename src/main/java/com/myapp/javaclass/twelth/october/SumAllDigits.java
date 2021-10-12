838c71e152247190eccd7ee117bd79d989f8d0ff
Manchala Gurunadha Rao <manachala.gurundharao331@gmail.com>
2021-10-12 21:20:14 +0530

    guru new

--- src/main/java/com/myapp/javaclass/twelth/october/SumAllDigits.java
+++ src/main/java/com/myapp/javaclass/twelth/october/SumAllDigits.java
@@ -4,14 +4,16 @@ public class SumAllDigits {
 
 	public static void main(String[] args) {
 		SumAllDigits sad = new SumAllDigits();
-		Integer sumValue = sad.getSumOfAllDigits(432543645);
+		Integer sumValue = sad.getSumOfAllDigits(4325);
+		//4+3+2+5+4+3+6+4+5 = 36
 		System.out.println("final sumValue ---->"+sumValue);
 	}
 	
 	private Integer getSumOfAllDigits(Integer number) {
 		String num = String.valueOf(number);
 		Integer sum = 0;
-		for(int count=0; count < num.length(); count++) {
+		Integer length = num.length();
+		for(int count=0; count < length ; count++) {
 			String s = String.valueOf(num.charAt(count));
 			sum = sum + Integer.valueOf(s);
 		}
